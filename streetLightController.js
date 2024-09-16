24f1d76a81ee22bef58b59ddf15dc17a22ab4d57
BhanuJamwal <bhanujamwal25@gmail.com>
2024-09-16 16:49:49 +0530

    fix interval

--- streetLightController.js
+++ streetLightController.js
@@ -7,7 +7,7 @@ const brokerUrl = 'mqtts://dmp-tata.orbiwise.com:8883';
 const topicBase = 'streetlight/controller';
 
 // Define the number of street light controllers (SLC devices)
-const Devices = [{"deviceId":"SLCILM0001","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD"},{"deviceId":"SLCILM0002","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD"},{"deviceId":"SLCILM0003","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD"},{"deviceId":"SLCILM0004","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD"},{"deviceId":"SLCILM0005","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD"}];
+const Devices = [{"deviceId":"SLCILM0001","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD","number":1},{"deviceId":"SLCILM0002","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD","number":2},{"deviceId":"SLCILM0003","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD","number":3},{"deviceId":"SLCILM0004","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD","number":4},{"deviceId":"SLCILM0005","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD","number":5}];
 //const Devices = [{"deviceId":"SLCILM0001","username":"3UUBsUUZa7DgVbheMX8jrDAB0","password":"Dyfs5rI3gMYWyDt76SKKmqOqLqOD"}];
 
 // Array to store each SLC device
@@ -22,9 +22,9 @@ function initializeDevice(device) {
   // Each SLC device has its own status and brightness
   const slcDevice = {
     id: device.deviceId,
-    status: 'OFF',
-    brightness: 0,
-    client,
+    // status: 'OFF',
+    // brightness: 0,
+    client
   };
 
   client.on('connect', () => {
@@ -64,9 +64,10 @@ function initializeDevice(device) {
 // Function to send a status update for each device
 function sendStatusUpdate(device, topic) {
   const statusMessage = JSON.stringify({ 
-    id: device.id, 
-    status: device.status, 
-    brightness: device.brightness 
+    deveui: device.id, 
+    //status: device.status, 
+    //brightness: device.brightness,
+    ...data
   });
 
   // Publish status update to the broker
@@ -130,21 +131,20 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Port = 10
-// Packet = Device Status Change Message
-// Regular Message Counter = 22296
-// AC Mains Status = true
-// Load On Off Status = false
-// Dimming Value = 0
-// Lux Sensor Value = 0
-// CT Current Value = 255
-// Device Mode = 2
-// Device Dimming Profile = 0
-// Device Calendar Profile = 1
-// Device Mode Description = Schedule Mode
-// Device Dimming Profile Description = No Dimming Applied
-// Device Calendar Profile Description = User Calendar 1
-// Controller Running Hour Counter = 4913
+const data ={"Port":10,
+"Packet":"Device Status Change Message",
+"AC Mains Status":true,
+"Load On Off Status" : false,
+"Dimming Value": 0,
+"Lux Sensor Value" : 0,
+"CT Current Value" : 255,
+//"Device Mode" : 2,
+//"Device Dimming Profile" : 0,
+//"Device Calendar Profile" : 1,
+//"Device Mode Description" : "Schedule Mode",
+//"Device Dimming Profile Description" : "No Dimming Applied",
+//"Device Calendar Profile Description" : "User Calendar 1",
+// Controller Running Hour Counter : 4913
 // Controller Running Hour Minutes = 12
 // Load Burn Hour Counter = 1705
 // Load Burn Hour Minutes = 39
@@ -152,22 +152,23 @@ server.listen(PORT, () => {
 // Load OFF Event Counter = 299
 // Alarm Message Counter = 1178
 // Device SW Reset Counter = 516
-// Metering Energy kWH Counter = 25.25
-// Burning Hours = 1705.65
-// Voltage Value = 229.33
-// Current Value = 0
-// Active Power Value = 0.00
-// Reactive Power Value = 0
-// Frequency = 50
-// Power Factor Value = 0
-// Active Energy Value = 25.25
-// Apparent Power Value = 0
-// Apparent Energy Value = 0.00
-// Reactive Energy Value = 0.00
+"Metering Energy kWH Counter" : 25.25,
+"Burning Hours" : 1705.65,
+"Voltage Value" : 229.33,
+"Current Value" : 0,
+"Active Power Value" : 0.00,
+"Reactive Power Value" : 0,
+"Frequency" : 50,
+"Power Factor Value": 0,
+"Active Energy Value" : 25.25,
+"Apparent Power Value" : 0,
+"Apparent Energy Value" : 0.00,
+"Reactive Energy Value" : 0.00,
 // RSSI = 0
 // SNR = 0
-// Time Date = 17:42:49 13-09-2024
-// Battery Percentage = 2
-// Battery Value = 2
-// timestamp = 2024-09-13T12:17:47.735Z
-// deveui = 3cc1f605000601c1
+"Time Date" : "17:42:49 13-09-2024",
+"Battery Percentage" : 2,
+"Battery Value" : 2,
+"timestamp" : "2024-09-13T12:17:47.735Z",
+//"deveui" : "3cc1f605000601c1"
+}
